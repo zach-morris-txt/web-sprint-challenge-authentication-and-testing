0cff06de3fe123869c61da2f0ddb63ccbe78eaf0
zach-morris-txt <zach-morris@lambdastudents.com>
2021-09-17 10:53:48 -0400

    Scaffold

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+//Imports 
+require("dotenv").config();
 const server = require('./api/server.js');
 
+
+//Environment Variables
 const PORT = process.env.PORT || 3300;
+
+
+
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
