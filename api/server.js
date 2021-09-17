6279009ffb6f00f63021e6642e0ae8e3d40b746a
zach-morris-txt <zach-morris@lambdastudents.com>
2021-09-17 11:05:13 -0400

    Routers Connect, Not Built-Out

--- api/server.js
+++ api/server.js
@@ -1,19 +1,36 @@
+//Imports
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
 const restrict = require('./middleware/restricted.js');
-
 const authRouter = require('./auth/auth-router.js');
 const jokesRouter = require('./jokes/jokes-router.js');
 
+
+//Instance Of Express App
 const server = express();
 
+
+//Calling Middleware
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+
+//Consuming Routers
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
 
+
+//Error-Handling Middleware
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+
+//Exports; Exposing
 module.exports = server;
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = require('./api/server.js');
 const PORT = process.env.PORT || 3300;
 
 
-
+//Start
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
